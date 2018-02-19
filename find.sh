f803e0cfc7fddd1af78d1de4bca0cfe80b6d2bd2
Elijah Garcia <e_g257@txstate.edu>
2018-02-19 16:32:40 -0600

    Update find.sh

--- find.sh
+++ find.sh
@@ -7,9 +7,12 @@ read input
 
 RECORD=$(grep -i $input $DATABASE)
 
-if [ $? == 1 ]; #a '1' indicates that no matches were found
-	echo "Could not find the requested record"
-else
-	echo "Found the requested record: $RECORD"
-fi
+for line in "$RECORD"
+do
+	if [ $? == 1 ]; #a '1' indicates that no matches were found
+		echo "Could not find the requested record"
+	else
+		echo "Found the requested record: $line"
+	fi
+done
 }
