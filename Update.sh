d3be6889dc3631cfc769de8e762b875e029b4e9a
TheGreatHumble <36174488+TheGreatHumble@users.noreply.github.com>
2018-02-19 16:30:24 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-#echo "I am working"
 read -p "Please enter the first and last name on the file you wish to remove: " firstName
-##echo "$firstName is kewl" 
 grep  "^$firstName" ./dataBase.txt
 lnNmbr="grep -c \"^$firstName\" ./dataBase.txt "
 eval $lnNmbr
-read -p "Is this the file you wish to delete?" data
+read -p "Is this the file you wish to update?" data
 if test "$data" = "y" 
 	then grep -v "^$firstName" ./dataBase.txt > ./dataBaseHolder
-	read -p "Enter first and last name: " newName
-	read -p "Enter address: " newAdd
-	read -p "Enter phone: " newPhone
-	read -p "Enter email: " newEmail
+	read -p "Enter new first and last name: " newName
+	read -p "Enter new address: " newAdd
+	read -p "Enter new phone: " newPhone
+	read -p "Enter new email: " newEmail
 	echo "$newName:$newAdd:$newPhone:$newEmail" >> ./dataBaseHolder
 	mv ./dataBaseHolder ./dataBase.txt
 	echo "working"
