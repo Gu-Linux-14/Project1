af7896361ec1e2850dc768d16a96402d98be38a9
carterkindred33ck <C_k133@txstate.edu>
2018-02-19 19:55:50 -0600

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
 
-Record()
-{
+Record() {
 DATABASE="database.txt" 
 
 echo "Enter the data you want to find (any aspect of the record): " 
 read data 
 
-RECORD=$(grep -i $data $DATABASE) #Holds entire line (grep -i finds what you search for)
+RECORD=$(grep -c $data $DATABASE) #Holds entire line (grep -i finds what you search for)
+RECORDSTRING=$(grep -i $data $DATABASE)
+
 
 
 if [ $? == 0 ]; # $? returns the exit status of the previous command (grep -i), 
-		then   #0 means command worked and found the record from the user input
-		echo "Record Exists- $RECORD" 
-		echo "Deleting the line from the database... "
-		sed -i "" "/$RECORD/d" $DATABASE #this removes the record from the database
 
+then   #0 means command worked and found the record from the user input
+	if [ "$RECORD" -eq 1 ];
+	then 
+		echo "$RECORD"
+		echo "You have multiple records, do you want to delete them(y or n): "
+		read answer 
+			if [ "$answer" == "y" ] || [ "$answer" == "Y" ];
+			then
+				echo "within first if statement"
+				grep -v "$RECORDSTRING" $DATABASE > junkfile.txt
+				echo "delted all records"
+	 		else
+	 		echo "Duplicate variable, search a different field" 
+	 		fi
+	else 
+	echo "Just one string"	
+	echo "Record Exists- $RECORDSTRING" 
+	echo "Deleting the line from the database... "
+	grep -v "$RECORDSTRING" $DATABASE > junkfile.txt #this removes the record from the database
+	fi
 else 
-	echo "the record does not exist"
+echo "the record does not exist"
+fi
 
-fi 		
-		#grep -v "$LINE" $DATABASE > temp && mv temp $DATABASE
-		#this is an alternative to the sed command,removes the record as well if sed 
-		#doesnt work
+	 
+
+ 		
+		
 
 }
 
